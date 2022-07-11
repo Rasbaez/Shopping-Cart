a7a5d68ceacea67f0d1983bd614e8a1e9e834413
Roberto Baez <rbaezsp@gmail.com>
2022-07-11 17:25:22 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = async (computador) => {
   // seu código aqui
+  try {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${computador}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+      throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,36 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const item = require('../mocks/item');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchProducts should be a function', () => {
+    expect(typeof(fetchProducts)).toBe('function');
+  })
+
+  it('Test if fetch function is called one time', () => {
+    fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled()
+    }) 
+
+ it('Testing return of function "fetchProducts" called fetch one times with arg "computador"', () => {
+   expect(fetchProducts('computador')).toEqual(fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador'));
+ })
+
+
+ it('testing if function fetchProducts must be return an object Equal "computadorSearch"', async () => {
+  expect( await fetchProducts('computador')).toEqual(computadorSearch);
+})
+
+it('When fetchProducts called without params, need return an error msg "You must provide an url"', async () => {
+  expect.assertions(1);
+
+  try {
+    await fetchProducts();
+  } catch (error) {
+    expect(error).toEqual(new Error('You must provide an url'));
+  }
+});
+
 });
