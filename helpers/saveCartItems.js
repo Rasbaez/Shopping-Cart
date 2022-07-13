13b959cfece356eb17a0279d38a65beed55a9924
Roberto Baez <rbaezsp@gmail.com>
2022-07-13 17:23:55 -0300

    arrumando req 4, adicionando req 6, e req 7

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,8 @@
-const localStorageSimulator = require('../mocks/localStorageSimulator');
+// const localStorageSimulator = require('../mocks/localStorageSimulator');
 
-const saveCartItems = (element) => localStorage.setItem('cartItems', element);
+const saveCartItems = (cart) => {
+  localStorage.setItem('cartItems', cart);
+};
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;
--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-const items = document.querySelector('.items');
-const cart = document.querySelector('.cart__items');
+// const items = document.querySelector('.items');
+const ol = document.querySelector('.cart__items');
 const btnCartRemove = document.querySelector('.empty-cart');
+
+const cartItemClickListener = async (event) => {
+  // aqui eu tentei descontruir o target {target}, passando como parâmetro, mas não sei por que não funcionou.
+  event.target.remove();
+};
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+const cartItem = async (elemId) => {
+  const selectProduct = await fetchItem(elemId);
+  const { id, title, price } = selectProduct;
+  const itemObj = {
+    sku: id,
+    name: title,
+    salePrice: price,
+  };
+  const li = createCartItemElement(itemObj);
+  ol.appendChild(li);
+ console.log(ol);
+};
+
+const getItemId = ({ target }) => {
+  const itemId = target.parentElement.firstChild.innerText;
+  cartItem(itemId);
+};
+
+const selectedProduct = () => {
+  const btnCart = document.querySelectorAll('.item__add');
+  btnCart.forEach((btn) => { 
+    btn.addEventListener('click', getItemId);
+  }); 
+};
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -8,7 +47,6 @@ const createProductImageElement = (imageSource) => {
   img.src = imageSource;
   return img;
 };
-
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -28,51 +66,23 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-const cartItemClickListener = async (event) => {
-  // aqui eu tentei descontruir o target {target}, passando como parâmetro, mas não sei por que não funcionou. 
-  event.target.remove();
-};
-
-cart.addEventListener('click', cartItemClickListener);
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
-
-const addToCart = async (e) => {
-  const selectProduct = await e.target.parentNode.firstElementChild.innerText;
-   // foi difícil pensar que por conta da depêndencia do produto selecionado, precisamos do await, mas funcionou//
-  const product = await fetchItem(selectProduct);
-  // console.log(product);
-  const { id, title, price } = product;
-
- const addtoCart = createCartItemElement({ sku: id, name: title, salePrice: price });
- cart.appendChild(addtoCart);
-};
-
-items.addEventListener('click', addToCart);
-
 const createProductsList = async () => {
   const products = await fetchProducts('computador');
   const section = document.querySelector('.items');
-   products.results.forEach((elem) => {
+  const { results } = products;
+  results.forEach((elem) => {
     const productItem = createProductItemElement({
       sku: elem.id,
       name: elem.title,
       image: elem.thumbnail,
       price: elem.price,
-    });  
+    });
     section.appendChild(productItem);
-  }); 
+  });
 };
 
-window.onload = async () => { 
- await fetchProducts('computador');
- await createProductsList();
+window.onload = async () => {
+  await fetchProducts('computador');
+  await createProductsList();
+  selectedProduct();
 };
